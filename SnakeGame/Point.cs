b6f81cd02381ec641d765ae9b1ea0e927c0d2271
feelitbreak <kendyslesha@gmail.com>
2022-07-12 23:48:17 +0300

    added separate file for point class

---
+++ SnakeGame/Point.cs
@@ -0,0 +1,32 @@
+﻿namespace SnakeGame
+{
+    /// <summary>
+    /// Class for a point with coordinates x and y.
+    /// </summary>
+    internal class Point
+    {
+        private readonly int x;
+        private readonly int y;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Point"/> class.
+        /// </summary>
+        /// <param name="x">Coordinate x.</param>
+        /// <param name="y">Coordinate y.</param>
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        /// <summary>
+        /// Draws a specified char int the (x,y) position.
+        /// </summary>
+        /// <param name="c">The char to draw.</param>
+        public void DrawChar(char c)
+        {
+            Console.SetCursorPosition(this.x, this.y);
+            Console.Write(c);
+        }
+    }
+}
--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -14,23 +14,4 @@
             Console.ReadLine();
         }
     }
-
-    internal struct Point
-    {
-        public int X { get; set; }
-
-        public int Y { get; set; }
-
-        public Point(int x, int y)
-        {
-            this.X = x;
-            this.Y = y;
-        }
-
-        public void DrawChar(char c)
-        {
-            Console.SetCursorPosition(this.X, this.Y);
-            Console.Write(c);
-        }
-    }
 }
