5eeb87fe7c843812d2551648966b01b6350112ca
feelitbreak <kendyslesha@gmail.com>
2022-07-12 23:34:27 +0300

    separate class for borders with separate file and summaries

---
+++ SnakeGame/Borders.cs
@@ -0,0 +1,46 @@
+﻿namespace SnakeGame
+{
+    /// <summary>
+    /// Class for drawing the borders of the game.
+    /// </summary>
+    internal static class Borders
+    {
+        private const int CornerX = 85;
+        private const int CornerY = 25;
+        private const char CharOfBorders = '*';
+
+        /// <summary>
+        /// Draws the borders.
+        /// </summary>
+        public static void Draw()
+        {
+            DrawBorders(CornerX, CornerY);
+        }
+
+        private static void DrawBorders(int x, int y)
+        {
+            static void DrawHorizontal(int x, int y, int length)
+            {
+                for (int i = 0; i <= length; i++)
+                {
+                    Point p = new (x + i, y);
+                    p.DrawChar(CharOfBorders);
+                }
+            }
+
+            static void DrawVertical(int x, int y, int length)
+            {
+                for (int i = 0; i <= length; i++)
+                {
+                    Point p = new (x, y + i);
+                    p.DrawChar(CharOfBorders);
+                }
+            }
+
+            DrawHorizontal(0, 0, x);
+            DrawVertical(x, 0, y);
+            DrawHorizontal(0, y, x);
+            DrawVertical(0, 0, y);
+        }
+    }
+}
--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main()
         {
-            GameInterface.Draw();
+            Borders.Draw();
             Console.ReadLine();
         }
     }
@@ -25,44 +25,6 @@
         {
             Console.SetCursorPosition(this.X, this.Y);
             Console.Write(c);
-        }
-    }
-
-    internal static class GameInterface
-    {
-        private const int CornerX = 85;
-        private const int CornerY = 25;
-        private const char CharOfBorders = '*';
-
-        public static void Draw()
-        {
-            DrawBorders(CornerX, CornerY);
-        }
-
-        private static void DrawBorders(int x, int y)
-        {
-            static void DrawHorizontal(int x, int y, int length)
-            {
-                for (int i = 0; i <= length; i++)
-                {
-                    Point p = new(x + i, y);
-                    p.DrawChar(CharOfBorders);
-                }
-            }
-
-            static void DrawVertical(int x, int y, int length)
-            {
-                for (int i = 0; i <= length; i++)
-                {
-                    Point p = new(x, y + i);
-                    p.DrawChar(CharOfBorders);
-                }
-            }
-
-            DrawHorizontal(0, 0, x);
-            DrawVertical(x, 0, y);
-            DrawHorizontal(0, y, x);
-            DrawVertical(0, 0, y);
         }
     }
 }
